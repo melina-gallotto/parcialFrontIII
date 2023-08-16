bc367fcd0e99703dda1ff57db9c1eeb5993d0fe9
Melina <meli.gallotto@gmail.com>
2023-08-16 18:37:12 -0300

    parcialFrontendIII

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Card from "./Components/Card";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+    </>
+  );
+}
+
+export default App
